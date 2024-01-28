810cdbc97a6426c0a3420b0e9b7a31dea40179ce
unknown <Lukasz.Bielenin@microsoft.wsei.edu.pl>
2024-01-28 14:23:59 +0100

    kalkulator

--- Lab-7/exercises/ćwiczenie-1/main.js
+++ Lab-7/exercises/ćwiczenie-1/main.js
@@ -9,6 +9,100 @@ const selectDate = document.getElementById("effective-date");
 const error = document.getElementById("error");
 const calculateBtn = document.getElementById("calculateBtn");
 
+function switchTypeOfDeal(current) {
+    if(current === "BUY") return "SELL";
+    return "BUY";
+}
+
+async function getMoneyCodes() {
+    const response = await fetch("https://api.nbp.pl/api/exchangerates/tables/C/");
+    const json = await response.json();
+
+    const currencies = json[0].rates.map(rate => rate.code);
+
+    return currencies;
+  }
+
+  function populateMoneyCodes(moneyCodes) {
+
+    moneyCodes.forEach(code => {
+        let option = document.createElement("option");
+        option.text = code;
+        option.value = code;
+        inputCode.add(option);
+    })
+    let option = document.createElement("option");
+    option.text = "PLN";
+    option.value = "PLN";
+    inputCode.add(option);
+
+    moneyCodes.forEach(code => {
+        let option = document.createElement("option");
+        option.text = code;
+        option.value = code;
+        outputCode.add(option);
+    })
+
+    let option2 = document.createElement("option");
+    option2.text = "PLN";
+    option2.value = "PLN";
+    outputCode.add(option);
+  }
+function getUrl(input, date) {
+    return `http://api.nbp.pl/api/exchangerates/rates/C/${input}/${date}`
+}
+  async function  run() {
+
+    error.innerText = "";
+
+    let date = selectDate.value;
+    let url = getUrl(inputCode.value, date);
+    let response = await fetch(url);
+    if(response.status !== 200)
+    {
+        error.innerText = "NO DATA FOR THIS DATE!";
+        return;
+    }
+    let json = await response.json();
+    let baseConvertsion = isSell() ? json.rates[0].bid : json.rates[0].ask;
+
+
+    
+    let closeConversion;
+    if(outputCode.value === 'PLN') {
+        closeConversion = 1;
+    } else {
+        url = getUrl(outputCode.value, date);
+        response = await fetch(url);
+        if(response.status !== 200)
+        {
+            error.innerText = "NO DATA FOR THIS DATE!";
+            return;
+        }
+        json = await response.json();
+
+        closeConversion = isSell() ? json.rates[0].bid : json.rates[0].ask;
+    }
+
+    let final = (inputAmount.value * baseConvertsion) / closeConversion;
+    
+    outputAmount.innerText = `${final.toFixed(2)} ${outputCode.value}`;
+}
+
+async function main() {
+
+    let possbileMoneyCodes = await getMoneyCodes();
+    populateMoneyCodes(possbileMoneyCodes);
+    calculateBtn.onclick = run;
+    
+}
+function isSell() { return document.getElementById('bid').checked; };
+
+main();
+
+// populateMoneyCodes(possbileMoneyCodes);
+
+
 /**
  * Zaimplementuj kalkulator walutowy, który pobiera dane kursów z API NBP.
  * Adres:
